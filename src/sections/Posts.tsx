7fa4babbbcd0e67d0c39a9d8b4896064d743fe58
Dawood Faisal <dawood_faisal@yahoo.com>
2025-08-27 05:07:33 +0500

    9

--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -24,23 +24,47 @@ const Posts = () => {
   return (
     <section id="posts" className="posts">
       <div className="container" data-aos="fade-up">
-        {items &&
-          items.length > 0 &&
-          items.map(
-            (item: {
-              item: {
-                _id: string;
-                img: string;
-                category: string;
-                date: string;
-                title: string;
-                brief: string;
-                avatar: string;
-                author: string;
-              };
-            }) => <PostItemOne key={item._id} large={false} item={item} />
-          )}
+
+        <div className="row g-5">
+
+          <div className="col-lg-4"></div>
+
+          <div className="col-lg-8">
+
+            <div className="row g-5">
+
+              <div className="col-lg-4 border-start custom-border">
+                {items &&
+                  items.length > 0 &&
+                  items.map(
+                    (item: {
+                      item: {
+                        _id: string;
+                        img: string;
+                        category: string;
+                        date: string;
+                        title: string;
+                        brief: string;
+                        avatar: string;
+                        author: string;
+                      };
+                    }) => (
+                      <PostItemOne key={item._id} large={false} item={item} />
+                    )
+                  )}
+              </div>
+
+              <div className="col-lg-4"></div>
+              <div className="col-lg-4"></div>
+
+            </div>
+
+          </div>
+
+        </div>
+
       </div>
+
     </section>
   );
 };
